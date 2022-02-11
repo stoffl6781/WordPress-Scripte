b6e3b9c41522be1483afbe14036d3afa9903ed39
Christoph Purin <christoph@purin.at>
2022-02-11 08:44:42 +0100

    added Readme

--- ACF/AutoTermBasedOnACFField.php
+++ ACF/AutoTermBasedOnACFField.php
@@ -8,6 +8,16 @@
  *  V: 1.0.0 ~ 11.02.2022
  */
 
+ /**
+  * Readme
+  * This script automatically creates terms coming from an advanced custom field.
+  * The content is taken from a freely definable ACF field, converted to uppercase and shortened to the first character.
+  * The new term is created using this abbreviated character.
+  * The script can handle special characters and umlauts.
+  * A little logic avoids duplicates.
+  * The slug and the description could be optionally extended, I didn't consider these fields here - but they can be left out. 
+  */
+
 function update_custom_terms($post_id) {
 
     if ( 'tours' != get_post_type($post_id)) {
@@ -19,18 +29,18 @@ function update_custom_terms($post_id) {
     }
     
     
-    $get_last_name = get_field( "nachname" ); //Hier das abzugreifende ACF Feld einstetzen Beispiel: nachname
+    $get_last_name = get_field( "lastname" ); //Add your ACF name field here, example lastname
     $last_name = strtoupper($get_last_name);
     $firstCharacter = mb_substr($last_name, 0, 1);
     $term_title = $firstCharacter;
     $term_slug = $firstCharacter;
-    $taxonomy = 'destinations'; // Hier deine Taxonomy einsetzen Beispiel: destinations
+    $taxonomy = 'destinations'; // Hadd your Taxonomy here example: destinations
     
     $existing_terms = get_terms($taxonomy, array(
         'hide_empty' => false
         )
     );
-
+    // Logic for duplicate taxonomy
     foreach($existing_terms as $term) {
         
         if ($term->term_name == $term_title) {
@@ -39,11 +49,12 @@ function update_custom_terms($post_id) {
         }             
         
     }
+    // If Taxonomy not exist, add new according to ACF Field
     if (empty($terms)) {
         $tax_insert_id = wp_insert_term($term_title,$taxonomy );
         $terms = $tax_insert_id['term_id'];
     }
     wp_set_object_terms( $post_id ,$terms, $taxonomy );
 }
-
+// run on update post process
 add_action('save_post', 'update_custom_terms');
